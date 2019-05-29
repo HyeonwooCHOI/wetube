690357c762bdae0ef81350481fc8acca2adc6be5
Hyeonwoo <gusdn7269@gmail.com>
2019-05-29 16:02:20 +0900

    Middlewares

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("You are on my profile222");
 
-app.get("/", handleHome);
+const betweenHome = (req, res, next) => {
+  console.log("I am between");
+  next();
+};
+
+app.use(betweenHome);
+
+app.get("/", betweenHome, handleHome);
 
 app.get("/profile", handleProfile);
 
