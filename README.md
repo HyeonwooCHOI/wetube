dcd056d8ddfec21af5bc534f75c9b09c177a927e
Hyeonwoo <gusdn7269@gmail.com>
2019-05-29 16:30:48 +0900

    Middlewares Part 2 [cookie-parser, body-parser, helmet, morgan](R)

--- README.md
+++ README.md
@@ -14,7 +14,6 @@ git commit -m "Initial Commit"
 git push origin master
 
 #babel
-
 babel은 최신 javascript 언어를 표준으로 변환해주는것
 
 @babel/core @babel/node @babel/preset-env 설치
@@ -22,3 +21,15 @@ babel은 최신 javascript 언어를 표준으로 변환해주는것
 
 #dependecies랑 상관 없이 개발자에게만 편하게 해주는거 설치 할 때
 npm install nodemon -D
+
+#morgan
+log 정보를 얻을 수 있음(parameter에따라 정보양 달라짐)
+
+#helmet
+보안을 위한 것 / 설치 후 그냥 미들웨어로 한 번 실행되게 하면됨.
+
+#body-parser
+Form에 의해 정보가 받아질 경우 request Object 안에 가지고 있게 함
+
+#cookie-parser
+cookie에 유저 정보를 저장 => session을 다루기 위해
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -11,14 +16,13 @@ const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("You are on my profile222");
 
-const betweenHome = (req, res, next) => {
-  console.log("I am between");
-  next();
-};
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
-app.use(betweenHome);
-
-app.get("/", betweenHome, handleHome);
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
