c01f2e6bb04ff0dad860b9b726ac9d88fee9163a
Hyeonwoo <gusdn7269@gmail.com>
2019-06-01 13:21:16 +0900

    Join Contorller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,24 @@
-export const join = (req, res) => res.render("join", { pageTitle: "join" });
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+
+  res.render("join", { pageTitle: "join" });
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "login" });
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "logout" });
